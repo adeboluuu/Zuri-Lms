e177b1f2d9d9442a58a24f177a4ba3431fc31390
adeboluuu <adeboluwarinthomas@gmail.com>
2022-07-20 20:31:15 +0100

    Polls:Update Polls models and make migration

--- Polls/models.py
+++ Polls/models.py
@@ -7,9 +7,12 @@ from django.db import models
 
 
 class Question(models.Model):
-
     question_text= models.CharField(max_length=255)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.question_text
 
 
 class Choices(models.Model):
